cc4f1d20f1bd8d559e70d8d89cf176335fb54982
Florian Jourde <jourdeflorian@gmail.com>
2024-06-25 00:03:29 +0200

    Update blur effect on hover

--- components/Portfolio/page.tsx
+++ components/Portfolio/page.tsx
@@ -22,15 +22,15 @@ export default function Portfolio() {
                 exit={{ x: -10, opacity: 0 }}
                 transition={{ delay: index * .1, duration: .8 }}
                 key={artwork.name}
-                className="artwork relative mb-5 sm:mb-10"
+                className="artwork relative mb-5 sm:mb-10 z-0 overflow-hidden"
               >
-                <Link className="flex flex-col p-5 justify-end absolute bottom-0 text-white h-full w-full before:content-[''] before:top-0 before:absolute before:left-0 before:w-full before:h-full before:bg-[linear-gradient(360deg,_black,_transparent_50%)] before:z-0 [&>*]:z-10" href={{
+                <Link className="flex flex-col p-5 justify-end absolute bottom-0 text-white h-full w-full before:content-[''] before:top-0 before:absolute before:left-0 before:w-full before:h-full before:bg-[linear-gradient(360deg,_black,_transparent_50%)] before:z-0 [&>*]:z-10 before:pointer-events-none [&:hover+img]:blur" href={{
                   pathname: `/artworks/${convertToSlug(artwork.name)}`,
                 }}>
                   <p className={`text-2xl font-bold font-libre-baskerville`}>{artwork.name}</p>
                   <p className={`text-sm opacity-50 font-libre-baskerville`}>{artwork.artist.name}</p>
                 </Link>
-                <Image height={0} width={0} sizes="100vw" className='relative w-full h-auto -z-10' alt='' src={artwork.images.gallery} />
+                <Image height={0} width={0} sizes="100vw" className='relative w-full h-auto -z-10 transition-all duration-700' alt='' src={artwork.images.gallery} />
               </motion.li>
             </AnimatePresence>
 
